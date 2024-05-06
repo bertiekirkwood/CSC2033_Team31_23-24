e9c3883cf9eab1ff7568cba6f2bdb5d748ab8a21
bertiekirkwood <bertiekkirkwood@gmail.com>
2024-05-06 19:30:40 +0100

    Add authorised.html for navbar consistent with all pages for authorised users

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home')     # whether user is directed to index.html or home.html should depend on if they are signed in
 def index():
     return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    # credentials check here
+    return render_template("login.html")
 
 
 if __name__ == '__main__':

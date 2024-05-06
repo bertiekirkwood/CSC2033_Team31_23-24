ddfeae5d0910290c30e694864baca31b491863ce
bertiekirkwood <bertiekkirkwood@gmail.com>
2024-05-06 19:42:39 +0100

    Correct minor link inaccuracies

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-@app.route('/home')     # whether user is directed to index.html or home.html should depend on if they are signed in
+@app.route('/index')     # whether user is directed to index.html or home.html should depend on if they are signed in
 def index():
     return render_template("index.html")
 
@@ -11,6 +12,8 @@ def index():
 def login():
     # credentials check here
     return render_template("login.html")
+
+# TODO: Add a route for all pages referenced in the HTML templates, including the register page
 
 
 if __name__ == '__main__':
